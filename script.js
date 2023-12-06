afba063fb184aff14820832c7f16584bcaab41e7
Canizade2004 <nihad_caniyev@bk.ru>
2023-12-06 14:19:39 +0400

    commit 4

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-// let sec2Boxs = document.getElementById('sec2Boxs');
+let sec2Boxs = document.getElementById('sec2Boxs');
 
-// fetch("http://localhost:3000/robots")
-// .then(response => response.json())
-// .then(data =>{
-//     data.forEach(item => {
+fetch("http://localhost:3000/robots")
+.then(response => response.json())
+.then(data =>{
+    data.forEach(item => {
 
-//         sec2Boxs.innerHTML +=
+        sec2Boxs.innerHTML +=
 
-//             `<div class="sec2-box1">
-//             <img src="${item.image}" alt="image/robot"/>
-//             <p class="sec2-box1-p1">${item.name}</p>
-//             <p class="sec2-box1-p2">${item.description}</p>
-//             <button class="sec2-box1-btn"><a href = "./details.html?id=${item.id}" target="_blank" >VIEW DETAILS</a></button>
-//             </div>`;
+            `<div class="sec2-box1">
+            <img src="${item.image}" alt="image/robot"/>
+            <p class="sec2-box1-p1">${item.name}</p>
+            <p class="sec2-box1-p2">${item.description}</p>
+            <button class="sec2-box1-btn"><a href = "./details.html?id=${item.id}" target="_blank"
+            class = "sec2-box1-a" >VIEW DETAILS</a></button>
+            </div>`;
 
-//     });
-// })
+    });
+})
 
 // let navbar = document.getElementById('navbar');
 // let nav = document.getElementById('nav');
